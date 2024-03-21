deefc7cb9a09b983aec7209d14cc42ae90155fad
Yash Bhardwaj <yashpremsharma@gmail.com>
2024-03-21 23:41:58 +0530

    fix issue

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-// module.exports = nextConfig
-module.exports = {
-    basePath: '/luxuary_books_web',
-    assetPrefix: '/luxuary_books_web/',
-  }
+module.exports = nextConfig
